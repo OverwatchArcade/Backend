aa263a0ba57f88541cdbaed2722bbe371ea7e666
quinten@peels.me <quinten@peels.me>
2021-09-21 21:58:20 +0200

    gitsave

--- Src/Models/Contributor.cs
+++ Src/Models/Contributor.cs
@@ -23,6 +23,7 @@ namespace OWArcadeBackend.Models
         public int ContributionCount { get; set; }
         public DateTime? LastContributedAt { get; set; }
         public string? FavouriteContributionDay { get; set; }
+        public IEnumerable<DateTime>? ContributionDays { get; set; }
     }
 
     public class ContributorProfile
