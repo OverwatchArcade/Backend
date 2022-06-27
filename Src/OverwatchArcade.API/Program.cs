f17e121c2d25666c2ab1cc0350275f1cdd038e80
Quinten Peels <quinten@peels.me>
2022-06-27 21:07:46 +0200

    logging

--- Src/OverwatchArcade.API/Program.cs
+++ Src/OverwatchArcade.API/Program.cs
@@ -2,76 +2,96 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using NLog;
+using NLog.Web;
 using OverwatchArcade.API;
 using OverwatchArcade.Persistence;
 using OWArcadeBackend.Services.CachingService;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Add services to the container.
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-DependencyInjection.Other(builder.Services);
-DependencyInjection.AddServices(builder.Services);
-DependencyInjection.AddFactories(builder.Services);
-DependencyInjection.AddValidators(builder.Services);
-DependencyInjection.AddRepositories(builder.Services);
-DependencyInjection.ConfigureCorsPolicy(builder.Services);
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
 
-builder.Services.AddAuthentication(option =>
-    {
-        option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    DependencyInjection.Other(builder.Services);
+    DependencyInjection.AddServices(builder.Services);
+    DependencyInjection.AddFactories(builder.Services);
+    DependencyInjection.AddValidators(builder.Services);
+    DependencyInjection.AddRepositories(builder.Services);
+    DependencyInjection.ConfigureCorsPolicy(builder.Services);
+
+    builder.Services.AddAuthentication(option =>
         {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-                .GetBytes(builder.Configuration.GetValue<string>("Jwt:Token"))),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+            option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                    .GetBytes(builder.Configuration.GetValue<string>("Jwt:Token"))),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
 
-builder.Services.AddDbContextPool<AppDbContext>(opt =>opt.UseSqlServer(builder.Configuration["Database:OWArcade"]));
+    builder.Services.AddDbContextPool<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration["Database:OWArcade"]));
 
-var app = builder.Build();
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-using (var scope = ((IApplicationBuilder)app) .ApplicationServices.CreateScope())
-{
-    await using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+    if (app.Environment.IsDevelopment())
     {
-        context?.Database.Migrate();
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
-    var cacheWarmupService = scope.ServiceProvider.GetService<ICacheWarmupService>();
-    await cacheWarmupService.Run();
+
+    using (var scope = ((IApplicationBuilder)app).ApplicationServices.CreateScope())
+    {
+        await using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+        {
+            context?.Database.Migrate();
+        }
+
+        var cacheWarmupService = scope.ServiceProvider.GetService<ICacheWarmupService>();
+        await cacheWarmupService.Run();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.UseCors();
+
+    app.UseResponseCaching();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.UseCors();
-
-app.UseResponseCaching();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
